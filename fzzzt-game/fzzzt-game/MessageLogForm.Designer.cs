075f2ccea815f351e0caff0c25a060fb3691c32e
tamati <dev.feng.zhang@gmail.com>
2025-10-01 15:00:20 +1300

    Change the position of the message log form

--- fzzzt-game/fzzzt-game/MessageLogForm.Designer.cs
+++ fzzzt-game/fzzzt-game/MessageLogForm.Designer.cs
@@ -45,10 +45,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(469, 450);
             this.Controls.Add(this.messageBox);
+            this.Location = new System.Drawing.Point(1440, 0);
             this.Name = "MessageLogForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "MessageLogForm";
             this.ResumeLayout(false);
             this.PerformLayout();
